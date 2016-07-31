0f645d198eabf740c37ad30bc2210e3ae06acef2
Laszlo Tamas <laszlo.devil@gmail.com>
2016-07-31 19:27:41 +0200

    Thread handling

---
+++ WIWEdetect/Form1.Designer.vb
@@ -0,0 +1,112 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.ButtonScan = New System.Windows.Forms.Button()
+        Me.LabelStatus = New System.Windows.Forms.Label()
+        Me.TextBoxScanResult = New System.Windows.Forms.TextBox()
+        Me.TextBoxWIWE = New System.Windows.Forms.TextBox()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 2
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 85.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.ButtonScan, 0, 3)
+        Me.TableLayoutPanel1.Controls.Add(Me.LabelStatus, 0, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.TextBoxScanResult, 0, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.TextBoxWIWE, 0, 0)
+        Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 4
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 40.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(787, 350)
+        Me.TableLayoutPanel1.TabIndex = 0
+        '
+        'ButtonScan
+        '
+        Me.ButtonScan.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ButtonScan.Location = New System.Drawing.Point(3, 283)
+        Me.ButtonScan.Name = "ButtonScan"
+        Me.ButtonScan.Size = New System.Drawing.Size(662, 64)
+        Me.ButtonScan.TabIndex = 0
+        Me.ButtonScan.Text = "START"
+        Me.ButtonScan.UseVisualStyleBackColor = True
+        '
+        'LabelStatus
+        '
+        Me.LabelStatus.AutoSize = True
+        Me.LabelStatus.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.LabelStatus.Font = New System.Drawing.Font("Courier New", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.LabelStatus.Location = New System.Drawing.Point(3, 210)
+        Me.LabelStatus.Name = "LabelStatus"
+        Me.LabelStatus.Size = New System.Drawing.Size(662, 70)
+        Me.LabelStatus.TabIndex = 1
+        '
+        'TextBoxScanResult
+        '
+        Me.TextBoxScanResult.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBoxScanResult.Font = New System.Drawing.Font("Courier New", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.TextBoxScanResult.Location = New System.Drawing.Point(3, 73)
+        Me.TextBoxScanResult.Multiline = True
+        Me.TextBoxScanResult.Name = "TextBoxScanResult"
+        Me.TextBoxScanResult.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBoxScanResult.Size = New System.Drawing.Size(662, 134)
+        Me.TextBoxScanResult.TabIndex = 2
+        '
+        'TextBoxWIWE
+        '
+        Me.TextBoxWIWE.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBoxWIWE.Font = New System.Drawing.Font("Courier New", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.TextBoxWIWE.Location = New System.Drawing.Point(3, 3)
+        Me.TextBoxWIWE.Name = "TextBoxWIWE"
+        Me.TextBoxWIWE.Size = New System.Drawing.Size(662, 44)
+        Me.TextBoxWIWE.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(787, 350)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.TableLayoutPanel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents ButtonScan As Button
+    Friend WithEvents LabelStatus As Label
+    Friend WithEvents TextBoxScanResult As TextBox
+    Friend WithEvents TextBoxWIWE As TextBox
+End Class
---
+++ WIWEdetect/Form1.vb
@@ -0,0 +1,87 @@
+﻿Imports System.Threading
+Imports System.Diagnostics
+Public Class Form1
+    Private trd As Thread
+    Private Sub ButtonScan_Click(sender As Object, e As EventArgs) Handles ButtonScan.Click
+        LabelStatus.Text = "Keresés..."
+        TextBoxScanResult.Text = vbNullString
+        TextBoxWIWE.Text = vbNullString
+        trd = New Thread(AddressOf ThreadTask)
+        trd.IsBackground = True
+        Debug.Print("Thread starts: " & Now)
+        trd.Start()
+
+    End Sub
+
+    Private Sub ThreadTask()
+        'A BT kezelés külön threadben legyen, különben elsőbbséget élvez
+        'és nem enged a futás végéig semmit csinálni
+        Dim bc As New InTheHand.Net.Sockets.BluetoothClient()
+        Dim deviceArray As InTheHand.Net.Sockets.BluetoothDeviceInfo() = bc.DiscoverDevices()
+        Dim count As Integer = deviceArray.Length
+        Dim sText As String
+        Dim fullText As String
+        Dim hNap As String
+        Dim hSap As String
+        Dim hMac As String
+        Dim intWIWECount As Integer
+        sText = vbNullString
+        fullText = vbNullString
+        intWIWECount = 0
+        For i As Integer = 0 To count - 1
+            hNap = Hex(deviceArray(i).DeviceAddress.Nap)
+            hSap = Hex(deviceArray(i).DeviceAddress.Sap)
+            hMac = hNap & hSap
+
+            fullText = GetFullText(deviceArray(i).DeviceName, hNap, hSap)
+
+            Console.WriteLine(fullText)
+            Debug.Print(fullText)
+            Me.SetScanText(fullText)
+            'TextBoxScanResult.Text = fullText
+        Next
+        'LabelStatus.Text = ""
+        Me.SetLabelText("")
+        'LabelStatus.Text = "Keresés..."
+    End Sub
+    Private Function GetFullText(device_name As String, device_nap As String, device_sap As String) As String
+        Dim res As String
+        res = "---" & Now.ToString & "-----------------------" & vbCrLf
+        res = res & device_name & vbCrLf
+        res = res & device_nap & device_sap & vbCrLf
+        Return res
+    End Function
+    Private Function IsWIWE(device_name As String, device_nap As String) As Boolean
+        Dim res As Boolean
+        res = False
+
+        Return res
+    End Function
+
+
+    'Threadsafe control handling
+    Delegate Sub SetTextCallback([text] As String)
+    Delegate Sub SetLabelTextCallback([text] As String)
+    Private Sub SetScanText(ByVal [text] As String)
+        ' InvokeRequired required compares the thread ID of the
+        ' calling thread to the thread ID of the creating thread.
+        ' If these threads are different, it returns true.
+        If Me.TextBoxScanResult.InvokeRequired Then
+            Dim d As New SetTextCallback(AddressOf SetScanText)
+            Me.Invoke(d, New Object() {[text]})
+        Else
+            Me.TextBoxScanResult.Text = [text]
+        End If
+    End Sub
+    Private Sub SetLabelText(ByVal [text] As String)
+        ' InvokeRequired required compares the thread ID of the
+        ' calling thread to the thread ID of the creating thread.
+        ' If these threads are different, it returns true.
+        If Me.LabelStatus.InvokeRequired Then
+            Dim d As New SetLabelTextCallback(AddressOf SetLabelText)
+            Me.Invoke(d, New Object() {[text]})
+        Else
+            Me.LabelStatus.Text = [text]
+        End If
+    End Sub
+End Class
